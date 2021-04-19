94cf75083c422743cc95e31eb70ca3ca626517de
hari <hari@localhost.localdomain>
2021-04-20 02:21:48 +0530

    added more instances

--- main.tf
+++ main.tf
@@ -21,10 +21,25 @@ provider "aws" {
     region  = "ap-southeast-1"
 }
 
+# modules to launch ec2 instances in various regions
 module "ec2_instance" {
     source = "./modules/ec2"
     providers = {
         aws.region = aws.us
+    }
+}
+module "ec2_instance2" {
+    source = "./modules/ec2"
+    providers = {
+        aws.region = aws.mumbai
+    }
+
+}
+
+module "ec2_instance3" {
+    source = "./modules/ec2"
+    providers = {
+        aws.region = aws.singapore
     }
 
 }
